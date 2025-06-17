28bfae5aa55405b93b24f48f5cf0f36e3fa70d8f
s2990164 <n.p.damink@student.utwente.nl>
2025-06-17 16:12:39 +0200

    Removed the loading, since it was distracting

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -15,9 +15,9 @@ export const LoadingScreen = ({onComplete}) => {
                 
                 setTimeout(() => {
                     onComplete();
-                }, 500);
+                }, 400);
             }
-        }, 70);
+        }, 60);
 
         return () => clearInterval(interval);
     }, [onComplete]);
@@ -27,12 +27,6 @@ export const LoadingScreen = ({onComplete}) => {
             <div className="mb-4 text-4xl font-mono font-bold">
                 {text} <span className="animate-blink ml-1"> | </span>
             </div>
-
-            <div className="w-[400px] h-[4px] bg-gray=800 rounded relative overflow-hidden">
-                <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
-                </div>
-            </div>
-
         </div>
     );
 };
