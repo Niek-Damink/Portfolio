b6f7f2cf8ebf2bf522316d4cb7c68f81da967943
s2990164 <n.p.damink@student.utwente.nl>
2025-06-17 15:50:55 +0200

    Removed it again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import logo from '../assets/Niek_Damink.png';
 
 export const Navbar = ({menuOpen, setMenuOpen}) => {
 
@@ -11,8 +10,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white flex items-center space-x-3"> 
-                        <img src={logo} alt="Logo" className="w-8 h-8" />
+                    <a href="#home" className="font-mono text-xl font-bold text-white"> 
                         <span className="bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">Niek Damink</span> 
                     </a>
 
