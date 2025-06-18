2a2599e23b18c76861e902a724c46dc97e5ae422
s2990164 <n.p.damink@student.utwente.nl>
2025-06-18 21:47:36 +0200

    Changed the icons from lightbulb to moon/sun

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { FaLightbulb } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import { HiSun } from "react-icons/hi";
 
 export const Navbar = ({menuOpen, setMenuOpen, isDark, setIsDark}) => {
 
@@ -9,19 +10,11 @@ export const Navbar = ({menuOpen, setMenuOpen, isDark, setIsDark}) => {
 
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-            <div className="max-w-5xl mx-auto px-4">
-                <div className="grid grid-cols-3 items-center h-16">
+            <div className="w-full px-4">
+                <div className="grid grid-cols-2 md:grid-cols-3 items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white justify-self-start"> 
                         <span className="bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent leading-right">Niek Damink</span> 
                     </a>
-
-                    <div 
-                    className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
-                    onClick={() => setMenuOpen((prev) => !prev)}
-                    >
-                        &#9776;
-                    </div>
-
                     <div className="hidden md:flex items-centers space-x-8 justify-self-center">
                         <a 
                             href="#home"
@@ -50,16 +43,29 @@ export const Navbar = ({menuOpen, setMenuOpen, isDark, setIsDark}) => {
                         </a>
                     </div>
 
-                    <a 
-                        className="justify-self-end"
-                        onClick={() => {
-                            setIsDark((prev) => !prev);
-                            document.documentElement.classList.toggle("light");
-                        }}
-                    >
-                        <FaLightbulb className={`text-2xl ${isDark ? "text-gray-300 hover:text-yellow-300" : "text-yellow-300 hover:text-gray-300"} hover:cursor-pointer`} />
-                    </a>
-
+                    <div className="justify-self-end flex items-center gap-x-5">
+                        <div 
+                        className="text-2xl cursor-pointer z-40 md:hidden text-primaryText"
+                        onClick={() => setMenuOpen((prev) => !prev)}
+                        >
+                            &#9776;
+                        </div>
+                        <a 
+                            className="border-2 border-primaryText/40 p-1.5 rounded-xl"
+                            onClick={() => {
+                                setIsDark((prev) => !prev);
+                                document.documentElement.classList.toggle("light");
+                            }}
+                        >
+                            {
+                                isDark ? (
+                                    <FaMoon className="text-2xl hover:cursor-pointer text-primaryText"/>
+                                ) : (
+                                    <HiSun className="text-2xl hover:cursor-pointer text-primaryText/80"/>
+                                )
+                            }
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
