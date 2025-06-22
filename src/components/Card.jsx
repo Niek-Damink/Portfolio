7e05f8e533585fe2000521e83b213d451d2bc440
s2990164 <n.p.damink@student.utwente.nl>
2025-06-22 20:02:59 +0200

    Added photos to the projects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-export const Card = ( {children} ) => {
+export const Card = ( {photo, children} ) => {
 
     return (
-        <div className="p-6 rounded-xl border-4 bg-cardBackground border-titleText/10 hover:-translate-y-1 hover:border-blue-500/30 shadow-xl shadow-blue-500/20 hover:shadow-blue-500/40 transition-all duration-500">
-            {children}
+        <div className="rounded-xl border-4 bg-cardBackground border-titleText/10 hover:-translate-y-1 hover:border-blue-500/30 shadow-xl shadow-blue-500/20 hover:shadow-blue-500/40 transition-all duration-500">
+            {
+                photo != null ? <img className="rounded-t-lg h-60 object-cover w-full" src={photo} /> : <></>
+            }
+            <div className="p-6">
+                {children}
+            </div>
         </div>
     )
 }
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -2,24 +2,33 @@ import { FaYoutube, FaGithub, FaBookOpen, FaGlobe} from 'react-icons/fa';
 import { RevealOnScroll } from '../RevealOnScroll';
 import { Card } from "../Card";
 
+import portofolio from '../../assets/portofolio.png';
+import task_scheduling from '../../assets/task_scheduling.png';
+import MerlinRoads from '../../assets/MerlinRoads.png';
+import PokerPals from '../../assets/PokerPals.png';
+
 const projects = [
     {
         title: "MerlinRoads", 
-        technologies:["Python", "Dash", "Dash Sylvereye", "SUMO", "HERE/TomTom/Maps APIs"], 
+        photo: MerlinRoads,
+        technologies:["Python", "Dash", "Weaverlet", "SUMO", "HERE/TomTom/Maps APIs"], 
         links:[{icon:FaGithub, link:"https://github.com/J-a-c-c-o/MerlinRoads", name:"Github"}, {icon:FaYoutube, link:"https://youtu.be/WXPFHcs8VAs", name:"Overview"}], 
         description:"MerlinRoads is an interactive dashboard for visualizing and analyzing road incident data on urban road networks. It combines real-time and historical data with features like map filtering, SUMO-based traffic simulation, and visual analytics to support data-driven decisions."
     }, {
         title: "LBC Task Scheduling Heuristic", 
+        photo: task_scheduling,
         technologies:["Python", "R", "IGraph", "Jupyter Notebook", "Data Visualization"], 
         links:[{icon:FaGithub, link:"https://github.com/Niek-Damink/LBC_TaskSched", name:"Github"}, {icon:FaBookOpen, link:"/Research_Project_Paper.pdf", name:"Read"}], 
         description:"This project, developed as part of my thesis, introduces task scheduling heuristics based on Longest Betweenness Centrality to improve efficiency in distributed systems. The heuristics are evaluated against established methods using synthetic task graphs."
     }, {
         title: "PokerPals", 
+        photo: PokerPals,
         technologies:["Django", "TailwindCSS", "Python", "JavaScript"], 
         links:[{icon:FaGithub, link:"https://github.com/Niek-Damink/PokerPals", name:"Github"}, {icon:FaGlobe, link:"https://pokerpals.nl", name:"Visit"}], 
         description:"PokerPals is a poker statistics website built for my friend group. It features player accounts, team chats, and awards like most wins or losses. It also tracks game history, detailed player stats, and total earnings across all sessions."
     }, {
         title: "Portofolio", 
+        photo: portofolio,
         technologies:["React", "TailwindCSS", "JavaScript"], 
         links:[{icon:FaGithub, link:"https://github.com/Niek-Damink/portofolio", name:"Github"}, {icon:FaGlobe, link:"https://niekdamink.com", name:"Visit"}], 
         description:"This website serves as my personal portfolio, where I share a selection of projects I have worked on, along with an overview of the skills I have developed and the experiences I have gained through various learning and work opportunities."
@@ -43,7 +52,7 @@ export const Projects = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     {projects.map((project, key) => (
                     <RevealOnScroll key={key} direction={key % 2 === 0 ? 'left' : 'right'}>
-                        <Card>
+                        <Card photo={project.photo}>
                             <h3 className="text-xl font-bold mb-2 text-blue-500"> {project.title} </h3>
                             <p className="text-primaryText transition-all duration-500"> {project.description}</p>
                             <div className='flex flex-wrap gap-3 my-4'>
