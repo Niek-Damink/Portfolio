0a7b4696f03b9d84ab5029a5bb0bc84e56909ffa
s2990164 <n.p.damink@student.utwente.nl>
2025-09-17 12:59:28 +0200

    Updated CV download to work with github pages

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -25,7 +25,7 @@ export const Home = () => {
                         <a href="#about" className="bg-blue-500/30 md:hidden text-blue-500 py-3 px-6 rounded-l-full font-medium transition flex items-center justify-center  relative overflow-hidden hover:-translate-y-0.5 drop-shadow-xl drop-shadow-blue-500/30 hover:drop-shadow-blue-500/80">
                             More
                         </a>
-                        <a href="/CV.pdf" download="CV.pdf" className='bg-blue-500/30 text-blue-500 py-3 px-6 rounded font-medium flex items-center gap-x-2 hover:-translate-y-0.5 drop-shadow-xl drop-shadow-blue-500/30 hover:drop-shadow-blue-500/80'>
+                        <a href={import.meta.env.BASE_URL+"CV.pdf"} download="CV.pdf" className='bg-blue-500/30 text-blue-500 py-3 px-6 rounded font-medium flex items-center gap-x-2 hover:-translate-y-0.5 drop-shadow-xl drop-shadow-blue-500/30 hover:drop-shadow-blue-500/80'>
                         CV
                         <FaDownload />
                         </a>
